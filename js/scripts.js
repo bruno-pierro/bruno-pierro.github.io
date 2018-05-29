f2b7b87d65e9b3e8e77e8f297770e7f155026077
Bruno Pierro <bruno.pierro@ti.supp.com.br>
2018-05-29 11:49:45 -0300

    scripts de controle

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+
+
+$('.event1Trigger').hover(function(){
+	$('.event1Bubble').css("visibility", "visible");
+},function(){
+    $('.event1Bubble').css("visibility", "hidden")
+});
+
+$('.event2Trigger').hover(function(){
+	$('.event2Bubble').css("visibility", "visible");
+},function(){
+    $('.event2Bubble').css("visibility", "hidden")
+});
+
+$('.event3Trigger').hover(function(){
+	$('.event3Bubble').css("visibility", "visible");
+},function(){
+    $('.event3Bubble').css("visibility", "hidden")
+});
+
+$('.agora').hover(function(){
+	$('.event4Bubble').css("visibility", "visible");
+},function(){
+    $('.event4Bubble').css("visibility", "hidden")
+});
+
+$('#time1').hover(function(){
+	$('.event1Bubble').css("visibility", "visible");
+},function(){
+    $('.event1Bubble').css("visibility", "hidden")
+});
+
+$('.time2').hover(function(){
+	$('.event2Bubble').css("visibility", "visible");
+},function(){
+    $('.event2Bubble').css("visibility", "hidden")
+});
+
+$('#time3').hover(function(){
+	$('.event3Bubble').css("visibility", "visible");
+},function(){
+    $('.event3Bubble').css("visibility", "hidden")
+});
+
+
+$('.fa-facebook-square').hover(function(){
+	$(this).css("font-size", "3em");
+},function(){
+    $(this).css("font-size", "2em")
+});
+
+$('.fa-twitter-square').hover(function(){
+	$(this).css("font-size", "3em");
+},function(){
+    $(this).css("font-size", "2em")
+});
+
+$('.fa-linkedin').hover(function(){
+	$(this).css("font-size", "3em");
+},function(){
+    $(this).css("font-size", "2em")
+});
+
+$('.fa-github-square').hover(function(){
+	$(this).css("font-size", "3em");
+},function(){
+    $(this).css("font-size", "2em")
+});
+
+
+
+
+})
+
+
