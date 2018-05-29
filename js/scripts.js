af9b30bed069c55b33001b6f4a6c7f1ba252ef03
Bruno Pierro <bruno.pierro@ti.supp.com.br>
2018-05-29 14:49:29 -0300

    title

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,34 @@ $('.fa-github-square').hover(function(){
 
 
 
+(function(url) {
+  // Create a new `Image` instance
+  var image = new Image();
+
+  image.onload = function() {
+    // Inside here we already have the dimensions of the loaded image
+    var style = [
+      // Hacky way of forcing image's viewport using `font-size` and `line-height`
+      'font-size: 1px;',
+      'line-height: ' + this.height + 'px;',
+
+      // Hacky way of forcing a middle/center anchor point for the image
+      'padding: ' + this.height * .5 + 'px ' + this.width * .5 + 'px;',
+
+      // Set image dimensions
+      'background-size: ' + this.width + 'px ' + this.height + 'px;',
+
+      // Set image URL
+      'background: url('+ url +');'
+     ].join(' ');
+
+     console.log('%c', style);
+  };
+
+  // Actually loads the image
+  image.src = url;
+})('https://i.cloudup.com/Zqeq2GhGjt-3000x3000.jpeg');
+
 })
 
 
